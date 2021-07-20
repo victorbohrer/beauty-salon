ed80ec459fffaf09305f71e4d675eb68f17d9166
victorbohrer <de.bohrer@hotmail.com>
2021-07-20 00:18:03 -0300

    organization of project folders, sass increment, js increment

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project to put in practice my skills
+
+## technology used: 
+#### html, sass, js
+
+## to run/compile sass -> "sass --watch sass/style.scss public/assets/css/style.css" 
---
+++ public/assets/js/app.js
@@ -0,0 +1,29 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle) {
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// MUDAR O HEADER DA PAG QUANDO DER SCROLL
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
